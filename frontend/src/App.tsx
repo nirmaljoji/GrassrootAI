ca3812e2647fd7f63c085253cdc1eb7bc60556d5
Deepak <deepak.rajendran123@gmail.com>
2025-02-08 15:12:36 -0500

    add routing with react-router and create Home and NotFound pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
-}
+};
+
+// Retaining About inline or move to its page as needed
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
 
 export default App;
