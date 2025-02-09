12c971aad17f4b42dbd916fa5c221183449af3de
Nirmal Joji <nirmalsharon.joji@gmail.com>
2025-02-08 21:05:22 -0500

    fixed file names

--- backend/agents/agents_framework.py
+++ backend/agents/agents_framework.py
@@ -21,11 +21,11 @@ options = members + ["FINISH"]
 
 
 system_prompt = (
-    "You are a supervisor tasked with managing a conversation between the"
-    f" following workers: {members}. Given the following user request,"
-    " respond with the worker to act next. Each worker will perform a"
-    " task and respond with their results and status. When finished,"
-    " respond with FINISH."
+  "You are a supervisor tasked with managing a conversation between the" +
+  " following workers: {members}. Given the following user request," +
+  " respond with the worker to act next. Each worker will perform a" +
+  " task and respond with their results and status. When finished," +
+  " respond with FINISH."
 )
 
 system_prompt = ChatPromptTemplate.from_messages(
